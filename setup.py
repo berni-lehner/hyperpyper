78eef8f5e751783120567a4743423ea2de835b65
Bernhard Lehner <71018765+berni-lehner@users.noreply.github.com>
2023-08-14 14:12:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='torch-transights',
+    name='transights',
     version='0.1.0',
     author='Bernhard Lehner',
     author_email='berni.lehner@gmail.com',
     description='Automatic collation of full batch output from transformation pipelines.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/berni-lehner/zippee-ki-yay',
+    url='https://github.com/berni-lehner/transights',
     project_urls = {
-        "Bug Tracker": "https://github.com/berni-lehner/torch-transights/issues"
+        "Bug Tracker": "https://github.com/berni-lehner/transights/issues"
     },
     license='Apache 2.0',
     install_requires=[],
