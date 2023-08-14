ad3beab8bac5ed490ce8c44132552fc5b21cec53
Bernhard Lehner <berni.lehner@gmail.com>
2023-08-14 14:04:25 +0200

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-import setuptools
+from setuptools import setup, find_packages
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name='torch-transights',
     version='0.1.0',
     author='Bernhard Lehner',
