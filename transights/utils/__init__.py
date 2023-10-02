b03410fb47672376fd57baa57b125f667d4da3a9
Bernhard Lehner <71018765+berni-lehner@users.noreply.github.com>
2023-10-02 07:35:37 +0200

    added PathList

--- transights/utils/__init__.py
+++ transights/utils/__init__.py
@@ -2,4 +2,5 @@ from .Pickler import Pickler
 from .FolderScanner import FolderScanner
 from .DataSetDumper import DataSetDumper
 from .EmbeddingPlotter import EmbeddingPlotter
+from .PathList import PathList
 from .PipelineCache import PipelineCache
