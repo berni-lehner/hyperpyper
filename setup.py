487afe38fb7748b202ac34a44ed50ff8a0405080
Bernhard Lehner <71018765+berni-lehner@users.noreply.github.com>
2024-02-18 12:48:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='hyperpyper',
-    version='0.1.2',
+    version='0.1.3',
     author='Bernhard Lehner',
     author_email='berni.lehner@gmail.com',
     description='Automatic collation of full batch output from transformation pipelines.',
