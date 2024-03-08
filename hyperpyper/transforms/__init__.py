424686225134764c9797b51118323b046c7c6aa0
Bernhard Lehner <71018765+berni-lehner@users.noreply.github.com>
2024-03-08 16:46:13 +0100

    Update __init__.py

--- hyperpyper/transforms/__init__.py
+++ hyperpyper/transforms/__init__.py
@@ -1,6 +1,6 @@
-from .img_transforms import PILToNumpy, NumpyToPIL, FileToPIL, DummyPIL, PILtoHist
-from .transforms import TensorToNumpy, FlattenArray, FlattenList, ReshapeArray
-from .transforms import ProjectTransform, FlattenTensor, SqueezeTensor
-from .transforms import ToDevice, ToArgMax, ToLabel
+from .img_transforms import PILToNumpy, NumpyToPIL, FileToPIL, DummyPIL, PILtoHist, PILTranspose
+from .transforms import FlattenArray, FlattenList, ReshapeArray
+from .transforms import ToArgMax, ToLabel
 from .transforms import DebugTransform, CachingTransform
-from .torch_transforms import PyTorchEmbedding, PyTorchOutput
+from .sklearn_transforms import ProjectTransform
+from .torch_transforms import PyTorchEmbedding, PyTorchOutput, FlattenTensor, SqueezeTensor, UnsqueezeTensor, TensorToNumpy, ToDevice
