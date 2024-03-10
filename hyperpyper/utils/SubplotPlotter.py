8fce6c97f4a8ecdf0e6f662664b23931cb947857
Bernhard Lehner <71018765+berni-lehner@users.noreply.github.com>
2024-03-10 10:41:41 +0100

    refactoring

--- hyperpyper/utils/SubplotPlotter.py
+++ hyperpyper/utils/SubplotPlotter.py
@@ -8,96 +8,121 @@ from matplotlib.figure import Figure
 class SubplotPlotter:
     def __init__(self,
                 n_subplots: int,
-                grid_layout: bool = True,
+                layout: str = 'auto',
                 rotate: bool = False,
-                title: Union[str, None] = None,
-                title_fontsize: Union[int, None] = None,
-                title_fontweight: Union[str, None] = None,
+                suptitle: Union[str, None] = None,
+                suptitle_fontsize: Union[int, None] = None,
+                suptitle_fontweight: Union[str, None] = None,
                 figsize: Union[Tuple[float, float], None] = None):
         """
         Constructs a SubplotPlotter object.
 
         Parameters:
-            grid_layout (bool): Whether to display the subplots as a grid.
+            n_subplots (int): The number of subplots.
+            layout (str): The layout mode for arranging subplots. Options are 'auto', 'grid', and 'vector'.
             rotate (bool): Whether to rotate the layout by 90 degrees.
-            title (str): Super title for the entire plot.
-            title_fontsize (int): Fontsize for the super title.
-            title_fontweight (str): Font weight for the super title.
+            suptitle (str): Super title for the entire plot.
+            suptitle_fontsize (int): Fontsize for the super title.
+            suptitle_fontweight (str): Font weight for the super title.
             figsize (tuple): The size of the entire figure in inches (width, height).
         """
         self.n_subplots: int = n_subplots
-        self.grid_layout: bool = grid_layout
+        self.layout = layout
         self.rotate: bool = rotate
-        self.title: Union[str, None] = title
-        self.title_fontsize: Union[int, None] = title_fontsize
-        self.title_fontweight: Union[str, None] = title_fontweight
+        self.suptitle: Union[str, None] = suptitle
+        self.suptitle_fontsize: Union[int, None] = suptitle_fontsize
+        self.suptitle_fontweight: Union[str, None] = suptitle_fontweight
         self.figsize: Union[Tuple[float, float], None] = figsize
 
         self.fig = None
         self.axes = None
 
+        self.n_rows = None
+        self.n_cols = None
+
         if self.n_subplots < 2: 
             raise ValueError(f"Invalid value for parameter n_subplots={self.n_subplots}.")
 
-        self.create_subplots()            
+        self._create_subplots()            
 
 
-#    def plot(self):
-#        """
-#        Draws the figures in corresponding subplots.
-#        """
-#        self.fig, self.axes = create_subplots()
-
-
-
-
-    def plot_finish(self) -> None:
-        if self.title is not None:
-            plt.suptitle(self.title, fontsize=self.title_fontsize, fontweight=self.title_fontweight)
+    def _finish_plot(self) -> None:
+        """
+        Finishes plot setup by setting the super title and adjusting layout.
+        """
+        if self.suptitle is not None:
+            plt.suptitle(self.suptitle, fontsize=self.suptitle_fontsize, fontweight=self.suptitle_fontweight)
             
         self.fig.tight_layout()
 
 
-    def create_subplots(self) -> None:
-        # Create the layout of subplots
-        if self.grid_layout:
-            self.create_grid()
+    def _create_subplots(self) -> None:
+        """
+        Creates subplot axes correponding to the given parameters.
+        """
+        if self.layout == 'grid':
+            self._create_grid()
+        elif self.layout == 'vector':
+            self._create_vector()
+        elif self.layout == 'auto':
+            self._create_autogrid()
         else:
-            if self.rotate:
-                nrows = 1
-                ncols = self.n_subplots
-            else:
-                nrows = self.n_subplots
-                ncols = 1
-            self.fig, self.axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=self.figsize)
+            raise ValueError(f"Invalid value for parameter layout={self.layout}.")
 
-            # Flatten the axes array for easier indexing
-            self.axes = self.axes.flatten()
+        if self.rotate:
+            tmp = self.n_rows
+            self.n_rows = self.n_cols
+            self.n_cols = tmp
+
+        self.fig, self.axes = plt.subplots(nrows=self.n_rows,
+            ncols=self.n_cols,
+            figsize=self.figsize)
+
+        self._finish_subplots()
 
 
-    def create_grid(self) -> None:
+    def _finish_subplots(self) -> None:
         """
-        Creates subplots in a grid layout.
-
-        This function calculates the number of rows and columns based on the desired number of subplots.
-        The subplots are then created using the `subplots` function from Matplotlib.
-
-        Returns:
-            matplotlib.figure.Figure: The generated figure object.
-            numpy.ndarray: Flattened array of axes objects representing the subplots.
+        Performs post-processing on subplots.
         """
-        square_len = np.sqrt(self.n_subplots)
-        # we sometimes need an additional row depending on the rotation and the number of subplots
-        row_appendix = int(bool(np.remainder(self.n_subplots,square_len))*self.rotate)
-
-        nrows = int(square_len) + row_appendix
-        ncols = int((self.n_subplots+nrows-1) // nrows)
-        
-        self.fig, self.axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=self.figsize)
-
         # Flatten the axes array for easier indexing
         self.axes = self.axes.flatten()
 
         # Remove axis and ticks for empty subplots
-        for i in range(self.n_subplots, nrows * ncols):
+        for i in range(self.n_subplots, self.n_rows * self.n_cols):
             self.axes[i].axis('off')
+
+
+    def _create_vector(self) -> None:
+        """
+        Creates subplot parameters aligned with a single axis.
+        """
+        self.n_rows = 1
+        self.n_cols = self.n_subplots
+
+
+    def _create_grid(self) -> None:
+        """
+        Creates subplot parameters strictly in a grid layout.
+        """
+        self.n_rows = self._find_smallest_square(self.n_subplots)
+        self.n_cols = self.n_rows
+
+
+    def _create_autogrid(self) -> None:
+        """
+        Creates subplot parameters in a grid layout while avoiding empty subplots.
+        For example, 8 subplots will result in a 4 by 2 subplot matrix instead of a 3 by 3 with an empty subplot.
+        """
+        square_len = np.sqrt(self.n_subplots)
+
+        self.n_rows = int(square_len)
+        self.n_cols = int((self.n_subplots+self.n_rows-1) // self.n_rows)
+
+
+    def _find_smallest_square(self, n):
+        """
+        Find the smallest square length to fit n elements.
+        """
+        smallest_square = int(np.ceil(np.sqrt(n))) ** 2
+        return int(np.sqrt(smallest_square))
