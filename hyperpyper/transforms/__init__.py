0ca2cb4a614fe3b2ff81eef42a469cf5b654d55b
Bernhard Lehner <71018765+berni-lehner@users.noreply.github.com>
2024-03-28 10:09:13 +0100

    added new img_transforms

--- hyperpyper/transforms/__init__.py
+++ hyperpyper/transforms/__init__.py
@@ -1,4 +1,6 @@
 from .img_transforms import PILToNumpy, NumpyToPIL, FileToPIL, DummyPIL, PILtoHist, PILTranspose
+from .img_transforms import BWToRandColor, GrayToRandColor, RandomPixelInvert, PixelInvert, PixelSet, DrawFrame
+from .img_transforms import JPEGCompressionTransform, WEBPCompressionTransform
 from .transforms import FlattenArray, FlattenList, ReshapeArray
 from .transforms import ToArgMax, ToLabel
 from .transforms import DebugTransform, CachingTransform
